bdca4da85ce67f1a33ed6e6496d1f0237c106af7
Loïc Gavens <loic.2010@live.fr>
2022-11-29 23:19:57 +0100

    added à l'approche

--- main.py
+++ main.py
@@ -23,13 +23,13 @@ url = 'https://prim.iledefrance-mobilites.fr/marketplace/stop-monitoring'
 # par votre clé API
 headers = {'Accept': 'application/json', 'apikey': secrets.api}
 # Envoi de la requête au serveur
-req = requests.get(url, headers=headers, params={'MonitoringRef': 'STIF:StopPoint:Q:58759:'})
+req = requests.get(url, headers=headers, params={'MonitoringRef': 'STIF:StopPoint:Q:22094:'})
 # cfo : 43114
 # cdg etoile rer A : 58759
 # Affichage du code réponse
 print('Status:', req)
 # Affichage du contenu de la réponse
-# print(req.content)
+print(req.content)
 req_json = req.json()
 valz = req.json()['Siri']['ServiceDelivery']['StopMonitoringDelivery'][0]['MonitoredStopVisit']
 newlist = sorted(valz, key=lambda d: d['MonitoredVehicleJourney']['MonitoredCall']['ExpectedArrivalTime'])
@@ -38,8 +38,12 @@ for x in newlist:
 
     train_name = vehicle['JourneyNote'][0]['value']
     dest_name = vehicle['DestinationName'][0]['value']
+    line_ref = vehicle['LineRef']['value']
     call_array = vehicle['MonitoredCall']
-    full_code = vehicle['VehicleJourneyName'][0]['value']
+    try:
+        full_code = vehicle['VehicleJourneyName'][0]['value']
+    except KeyError:
+        full_code = None
     date_pass = dateutil.parser.parser().parse(timestr=call_array['ExpectedArrivalTime'],
                                                tzinfos={'Z': dateutil.tz.gettz('Europe/London')}) \
         .astimezone(tz=dateutil.tz.gettz('Europe/Paris'))
@@ -48,22 +52,34 @@ for x in newlist:
         print(f"[{full_code}]", end=" ")
         if train_name == "UZEL":
             print(
-                f"Le {Fore.LIGHTRED_EX}RER (A){Fore.RESET} à destination de {Back.LIGHTRED_EX + dest_name + Back.RESET} passera en gare à {date_pass.strftime('%A %d/%m/%Y %H:%M:%S')}", end=' ')
+                f"Le {Fore.LIGHTRED_EX}RER (A){Fore.RESET} à destination de {Back.LIGHTRED_EX + dest_name + Back.RESET} passera en gare à {date_pass.strftime('%A %d/%m/%Y %H:%M:%S')}",
+                end=' ')
         elif train_name == "MOCA":
             print(
-                f"Le {Fore.LIGHTMAGENTA_EX}Transillien [L]{Fore.RESET} à destination de {dest_name} passera en gare à {date_pass.strftime('%A %d/%m/%Y %H:%M:%S')}", end=' ')
+                f"Le {Fore.LIGHTMAGENTA_EX}Transillien [L]{Fore.RESET} à destination de {dest_name} passera en gare à {date_pass.strftime('%A %d/%m/%Y %H:%M:%S')}",
+                end=' ')
         elif train_name == "PUCA":
             print(
-                f"Le {Fore.LIGHTMAGENTA_EX}Transillien [L]{Fore.RESET} à destination de {dest_name} passera en gare à {date_pass.strftime('%A %d/%m/%Y %H:%M:%S')}", end=' ')
+                f"Le {Fore.LIGHTMAGENTA_EX}Transillien [L]{Fore.RESET} à destination de {dest_name} passera en gare à {date_pass.strftime('%A %d/%m/%Y %H:%M:%S')}",
+                end=' ')
         elif train_name == "NANI":
             print(
-                f"Le {Fore.LIGHTRED_EX}RER (A){Fore.RESET} à destination de {dest_name} passera en gare à {date_pass.strftime('%A %d/%m/%Y %H:%M:%S')}", end=' ')
+                f"Le {Fore.LIGHTRED_EX}RER (A){Fore.RESET} à destination de {dest_name} passera en gare à {date_pass.strftime('%A %d/%m/%Y %H:%M:%S')}",
+                end=' ')
+        elif line_ref == "STIF:Line::C01372:":
+            print(
+                f"Le {Fore.LIGHTGREEN_EX}Métro (2){Fore.RESET} à destination de {Style.BRIGHT + Back.BLACK}{dest_name: <25}{Back.RESET + Style.NORMAL} passera en gare à {date_pass.strftime('%A %d/%m/%Y %H:%M:%S')}",
+                end=' ')
         else:
             print(
-                f"Le {Fore.LIGHTRED_EX}RER (A){Fore.RESET} à destination de {Style.BRIGHT + Back.BLACK}{dest_name: <25}{Back.RESET + Style.NORMAL} passera en gare à {date_pass.strftime('%A %d/%m/%Y %H:%M:%S')}", end=' ')
+                f"Le {Fore.LIGHTRED_EX}RER (A){Fore.RESET} à destination de {Style.BRIGHT + Back.BLACK}{dest_name: <25}{Back.RESET + Style.NORMAL} passera en gare à {date_pass.strftime('%A %d/%m/%Y %H:%M:%S')}",
+                end=' ')
 
-        #time
-        print(f"(dans {(time_diff.seconds//3600)*60 + (time_diff.seconds//60)%60} minutes)")
+        # time
+        if time_diff.seconds > 60:
+            print(f"(dans {(time_diff.seconds // 3600) * 60 + (time_diff.seconds // 60) % 60} minutes)")
+        else:
+            print(f"(À l'approche...)")
         # print(f"(dans {time_diff.seconds//3600, (time_diff.seconds//60)%60} minutes)")
 # print((x for x in req.json()['Siri']))
 # Ecriture de la réponse reçue sur un fichier
