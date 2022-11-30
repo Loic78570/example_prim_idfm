e3757194b44e05a85fe1964df1e98bfbeb89686c
Loïc Gavens <loic.2010@live.fr>
2022-11-30 11:10:37 +0100

    patched bug : unknown arrival time when available

--- main.py
+++ main.py
@@ -65,9 +65,13 @@ for x in newlist:
                                                    tzinfos={'Z': dateutil.tz.gettz('Europe/London')}) \
             .astimezone(tz=dateutil.tz.gettz('Europe/Paris'))
     except KeyError:
-        date_pass = dateutil.parser.parser().parse(timestr=call_array['AimedArrivalTime'],
-                                                   tzinfos={'Z': dateutil.tz.gettz('Europe/London')}) \
-            .astimezone(tz=dateutil.tz.gettz('Europe/Paris'))
+        try:
+            date_pass = dateutil.parser.parser().parse(timestr=call_array['AimedArrivalTime'],
+                                                   tzinfos={'Z': dateutil.tz.gettz('Europe/London')})\
+                .astimezone(tz=dateutil.tz.gettz('Europe/Paris'))
+        except KeyError:
+            # date_pass = date_dept
+            pass
     time_diff = (date_pass - datetime.datetime.now(tz=dateutil.tz.gettz('Europe/Paris')))
 
     if time_diff.seconds // 60 < 60:
